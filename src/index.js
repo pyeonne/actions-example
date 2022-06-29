a91891aa325a7593f7d35d1edf7ce47b2487eaaf
pyeonne <pyeonne@gmail.com>
2022-06-29 14:05:49 +0900

    📦 NEW: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // * Computes a factorial of a given positive integer
 function factorial(n) {
-  if (n < 0)
+  if (n < 0) {
     throw new Error("Factorial is only defined for non-negative integers!");
-  if (n === 0) return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
   return n * factorial(n - 1);
 }
 
